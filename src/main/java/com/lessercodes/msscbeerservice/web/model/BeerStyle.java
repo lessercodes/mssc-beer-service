fc3f96f8211b7fd78e872688e946046d4e413dbf
Szabó Csaba <lessercodes@gmail.com>
2022-10-18 19:17:49 +0000

    Add web data model. Closes #3

---
+++ src/main/java/com/lessercodes/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,14 @@
+package com.lessercodes.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON;
+}
