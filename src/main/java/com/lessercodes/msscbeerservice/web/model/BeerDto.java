b98144c5c720e00e5d5f09671bb8521274eacb53
lessercodes <lessercodes@gmail.com>
2022-11-01 21:09:36 +0100

    Add Jackson annotations to BeerDto's fields. Closes #13

--- src/main/java/com/lessercodes/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/lessercodes/msscbeerservice/web/model/BeerDto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,8 +26,10 @@ public class BeerDto {
     @Null
     private Integer version;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss2", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss2", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     @NotBlank
     private String beerName;
@@ -37,6 +40,7 @@ public class BeerDto {
     private Long upc;
     @Positive
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
     private Integer quantityOnHand;
 
